7f949d3866a7ea492629061d2a6f99fccc248ba5
quoctrungict01 <quoctrung.3hd@gmail.com>
2025-02-18 15:11:37 +0700

    create project + models

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express, { response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config()
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import connectDB from './config/connectDB.js';
+
+const app= express()
+app.use(cors({
+    credentials: true,
+    origin : process.env.FRONTEND_URL
+}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(helmet({
+    crossOriginResourcePolicy: false
+}));
+
+const PORT = 8080 || process.env.PORT
+
+app.get("/",(request, response)=>{
+    //server to client
+    response.json({
+        message:"server on running"+ PORT 
+    })
+})
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("server on running", PORT);
+})
+
+    
+})
