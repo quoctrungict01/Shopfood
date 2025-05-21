ebf72ed0f38fc5d27b93236e381f13d821fa0c76
quoctrungict01 <quoctrung.3hd@gmail.com>
2025-05-21 22:30:33 +0700

    complete

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import connectDB from './config/connectDB.js';
+import userRouter from './route/user.route.js'
 
 const app= express()
 app.use(cors({
@@ -28,6 +29,7 @@ app.get("/",(request, response)=>{
     })
 })
 
+app.use('/api/user',userRouter)
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
         console.log("server on running", PORT);
